fe1ec0085fb0682f6617aec2bf7d13e789bf1b68
Barasa <barasamica@gmail.com>
2024-02-09 02:12:50 +0300

    Created a Notfound .js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import RegisterBusinessPage from './components/RegisterBusinessPage';
-import DashboardPage from './components/DashboardPage';
+import DashboardPage from './components/dashboardPage';
 import SavingsPage from './components/SavingsPage';
+import NotFound from './components/NotFound'; 
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/register" component={RegisterBusinessPage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/savings" component={SavingsPage} />
-        </Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/register" component={RegisterBusinessPage} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/savings" component={SavingsPage} />
+        <Route component={NotFound} /> 
       </div>
     </Router>
   );
